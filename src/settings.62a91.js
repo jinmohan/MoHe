19491f519da6510a5e62e9baac1f431baa67238d
Aumh <160295555+jinmohan@users.noreply.github.com>
2024-07-13 12:23:04 +0800

    Add files via upload

---
+++ src/settings.62a91.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"5fa01",main:"b1b71"}};
